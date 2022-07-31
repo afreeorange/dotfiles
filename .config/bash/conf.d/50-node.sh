e3c67415ed901de5c5eb842368d714587f7fa20b
Nikhil Anand <mail@nikhil.io>
2022-07-31 10:02:15 -0500

    It's always worked that way.

--- .config/bash/conf.d/50-node.sh
+++ .config/bash/conf.d/50-node.sh
@@ -3,4 +3,5 @@
 # export NVM_DIR="$HOME/.nvm"
 
 # shellcheck source=/dev/null
-[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
