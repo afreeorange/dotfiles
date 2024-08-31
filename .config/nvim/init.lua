6b893845a3017d3784a4210adccba0330d8c5250
Nikhil Anand <mail@nikhil.io>
2024-08-31 10:40:33 -0500

    "I didn't create that part of the program." - 🤷

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,12 @@
-for _, source in ipairs {
-  "user.options",
-  "user.commands",
-  "user.key-mappings",
-  "user.plugin-loader",
-} do
-  local ok, fault = pcall(require, source)
-  if not ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
-end
+-- for _, source in ipairs {
+--   "user.options",
+--   "user.commands",
+--   "user.key-mappings",
+--   "user.plugin-loader",
+-- } do
+--   local ok, fault = pcall(require, source)
+--   if not ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+-- end
 
 -- =========== REFERENCES ========== --
 
