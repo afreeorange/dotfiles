1028c2a985187ef90708aa0046cf23692f817a67
Nikhil Anand <mail@nikhil.io>
2024-09-18 00:08:32 -0500

    "It's always worked that way." - 🤷

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,5 +11,17 @@ for _, source in ipairs {
     end
 end
 
+-- Enable syntax highlighting in fenced code blocks. Works with Treesitter.
+vim.g.markdown_fenced_languages = {
+  "bash",
+  "css",
+  "javascript",
+  "lua",
+  "python",
+  "scss",
+  "sql",
+  "typescript",
+}
+
 -- Set the color scheme here. It's a bit messy doing this via Lazy.vim
 pcall(vim.cmd.colorscheme, 'tokyonight')
