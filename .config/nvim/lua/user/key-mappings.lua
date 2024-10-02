ec5e6efc21d139167abe3998169bb1afbe33f05c
Nikhil Anand <mail@nikhil.io>
2024-10-02 17:58:08 -0500

    "I usually get a notification when that happens." - 🤷

--- .config/nvim/lua/user/key-mappings.lua
+++ .config/nvim/lua/user/key-mappings.lua
@@ -6,12 +6,15 @@ vim.keymap.set("n", "<leader>qq", "<cmd>qa!<CR>", { noremap = true, silent = tru
 vim.keymap.set("n", "<leader>qs", "<cmd>wqa<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>|", "<cmd>vsplit<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<C-n>", "<cmd>tabnew<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-[>", "<cmd>tabprevious<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-]>", "<cmd>tabnext<CR>", { noremap = true, silent = true })
--- TODO: Tab close...?
-
 if jit.os == "OSX" then
+
+  vim.keymap.set("n", "<C-n>", "<cmd>tabnew<CR>", { noremap = true, silent = true })
+  vim.keymap.set("n", "”", "<cmd>tabprevious<CR>", { noremap = true, silent = true })
+  vim.keymap.set("n", "’", "<cmd>tabnext<CR>", { noremap = true, silent = true })
+  -- TODO: Tab close...?
+
+
+
   vim.keymap.set("n", "Ω", "<cmd>set wrap!<cr>", { noremap = true, silent = true })
   vim.keymap.set("n", "¬", "<cmd>set nonumber!<cr>", { noremap = true, silent = true })
   vim.keymap.set("n", "œ", "<cmd>q<cr>", { noremap = true, silent = true })
