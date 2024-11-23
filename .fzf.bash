f90c0130ddc2c3578310054477890737a75c3b33
Nikhil Anand <mail@nikhil.io>
2024-11-23 15:44:13 -0600

    Fzf checkpoint

---
+++ .fzf.bash
@@ -0,0 +1,15 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+# shellcheck disable=SC1091
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2>/dev/null
+
+# Key bindings
+# ------------
+# shellcheck disable=SC1091
+source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
