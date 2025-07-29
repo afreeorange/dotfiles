bf4611eb729bc3b0c61468be2a8bd1117bed49fe
Nikhil Anand <mail@nikhil.io>
2025-07-29 08:51:43 -0500

    "I don't remember that in the original specification." - 🤷

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,5 +25,5 @@ vim.g.markdown_fenced_languages = {
 }
 
 -- Set the color scheme here. It's a bit messy doing this via Lazy.vim
-pcall(vim.cmd.colorscheme, 'tokyonight-night')
--- pcall(vim.cmd.colorscheme, 'gruvbox')
+-- pcall(vim.cmd.colorscheme, 'tokyonight-night')
+pcall(vim.cmd.colorscheme, 'gruvbox')
